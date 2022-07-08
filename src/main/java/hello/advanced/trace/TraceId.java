45966cd4a91d728a813d8a8865e2176f58d57aad
지금이야 <cz2123xc@gmail.com>
2022-07-08 22:30:41 +0900

    로그추적기 프로토타입 개발 & 테스트

---
+++ src/main/java/hello/advanced/trace/TraceId.java
@@ -0,0 +1,47 @@
+package hello.advanced.trace;
+
+import java.util.UUID;
+
+public class TraceId {
+
+    private String id;
+    private int level;
+
+    public TraceId() {
+        this.id = createId();
+        this.level = 0;
+    }
+
+    private TraceId(String id, int level) {
+        this.id = id;
+        this.level = level;
+    }
+
+    private String createId() {
+        return UUID.randomUUID().toString().substring(0, 8);
+    }
+
+    // 트랜잭션 아이디는 같고 레벨(들여쓰기)는 하나씩 증가 시키기 위해
+    public TraceId createNextId() {
+        return new TraceId(id, level + 1);
+    }
+
+    public TraceId createPreviousId() {
+        return new TraceId(id, level -1);
+    }
+
+    public boolean isFirstLevel() {
+        return level == 0;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+
+
+}
